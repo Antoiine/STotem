827fdd909d1fc6996ce5e0fb042d5bc2bb68569e
Antoiine <antoinemarinier@gmail.com>
2019-12-12 23:22:03 +0100

    [WIP] Board

---
+++ ClientApp/src/components/Totem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../css/Totem.css';
+
+export class Totem extends Component {
+    render() {
+        return (
+            <div className="totem">
+                <img src="/img/totem.jpg"/>
+            </div>
+        );
+    }
+}
--- ClientApp/src/components/TotemLine.js
+++ ClientApp/src/components/TotemLine.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import {Totem} from './Totem';
+
+import "../css/TotemLine.css";
 
 export class TotemLine extends Component {
+
+    
     render() {
+        const totems = [];
+        for(let i = 0; i< 9 ; i++) {
+            totems.push(<Totem key={i}/>);
+        }
         return (
             <div className="totemLine">
-                <h1>Border</h1>
+                {totems}
             </div>
         );
     }
